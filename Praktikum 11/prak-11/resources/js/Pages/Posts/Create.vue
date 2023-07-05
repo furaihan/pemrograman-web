d5eec8cadf95542209e3aa71e820d193d767e360
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-05 17:15:46 +0700

    prak 11 & 12

---
+++ Praktikum 11/prak-11/resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { useForm, Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Create Posts" />
+    <AuthenticatedLayout> </AuthenticatedLayout>
+</template>
