f975663dd0771a7db38756d9990fc785e361e458
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-15 20:24:25 +0700

    yuhuu

--- webprofile/resources/js/app.js
+++ webprofile/resources/js/app.js
@@ -5,14 +5,42 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+    faTiktok,
+    faInstagram,
+    faGithub,
+    faYoutube,
+    faFacebook,
+    faReddit,
+} from '@fortawesome/free-brands-svg-icons';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+    window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+library.add(
+    faCoffee,
+    faYoutube,
+    faFacebook,
+    faInstagram,
+    faGithub,
+    faReddit,
+    faTiktok,
+    faChevronRight
+);
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    title: title => `${title} - ${appName}`,
+    resolve: name =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue')
+        ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
+            .component('font-awesome-icon', FontAwesomeIcon)
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
